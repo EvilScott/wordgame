1819120aea269391c9ba9da888488a2b20e4a9ff
EvilScott <reis.robert.s@gmail.com>
2021-05-28 13:58:05 -0700

    add layout component

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import styles from 'styles/Layout.module.css';
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Layout({ children, title }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title ?? 'WordGame'}</title>
+      </Head>
+      <main className={styles.main}>
+        {children}
+      </main>
+      <footer className={styles.footer}>
+        <Link href={'/about'} shallow={true}><a>About</a></Link>
+        <span className={styles.separator}>//</span>
+        <a href={'https://github.com/EvilScott/wordgame'} target={'_blank'}>Source code</a>
+        <span className={styles.separator}>//</span>
+        <span>R. Scott Reis &copy; 2021</span>
+      </footer>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,19 @@
 import styles from 'styles/Home.module.css';
 import Game from 'components/Game';
-import Head from 'next/head';
-import { getLetters } from '../lib/api';
+import Layout from 'components/Layout';
+import { getLetters } from 'lib/api';
 
 export default function Home({ keyLetter, letters, possibleScore }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>WordGame</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>WordGame</h1>
-        <p className={styles.description}>
-          Spell as many words as you can with the following letters,<br />
-          but every word <i>must</i> use the <span className={'blue'}>blue</span> letter{' '}
-          and be at least 4 letters long.
-        </p>
-        <Game keyLetter={keyLetter} letters={letters} possibleScore={possibleScore} />
-      </main>
-
-      <footer className={styles.footer}>R. Scott Reis &copy; 2021</footer>
-    </div>
+    <Layout>
+      <h1 className={styles.title}>WordGame</h1>
+      <p className={styles.description}>
+        Spell as many words as you can with the following letters,<br />
+        but every word <i>must</i> use the <span className={'blue'}>blue</span> letter{' '}
+        and be at least 4 letters long.
+      </p>
+      <Game keyLetter={keyLetter} letters={letters} possibleScore={possibleScore} />
+    </Layout>
   );
 }
 
